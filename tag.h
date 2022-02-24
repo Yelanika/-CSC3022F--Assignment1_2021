bf7f981c13fac458a4c63baff69a45f28ef25f56
Yelanika Gunasekara <yelanika.gunasekara@gmail.com>
2022-02-24 21:37:20 +0200

    Completed TagStruct in tag.h

--- tag.h
+++ tag.h
@@ -1,6 +1,25 @@
+/***
+ * Created Date: 2022/02/24
+ * Yelanika Gunasekara
+ * GNSSEN002
+ * 
+ * tag.h 
+ * 
+ * ***/
+
+#ifndef tag.h
+#define tag.h
+
 #include <string>
 #include <vector>
 
 namespace GNSSEN002 {
 
+    struct TagStruct {
+        std::string tagName;
+        int noTagPairs;
+        std::string tagText;
+    };
 }
+
+#endif
