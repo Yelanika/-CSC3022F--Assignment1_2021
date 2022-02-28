3eff0566fabf03d754d355caf0da45a80fd991c2
Yelanika Gunasekara <yelanika.gunasekara@gmail.com>
2022-02-28 22:10:49 +0200

    Still busy - Extracting data into struct vector

--- tag.cpp
+++ tag.cpp
@@ -49,7 +49,37 @@ std::string GNSSEN002::ExtractTagsandText(std::vector<std::string> tagInfo) {
         text.push_back(tText);
     }
 
-    //std::cout << "Testing 1" << std::endl;
+    std::vector<GNSSEN002::TagStruct> Tag;
+    
+
+    Tag.push_back({tags[0], 1, text[0]});
+
+    if (tags.size() > 1) {
+        for (int i = 1; i < tags.size(); ++i) {
+            
+            bool found = false;
+            int k_tag;
+
+            //search for the same tag
+            for (k_tag = 1; k_tag < tags.size(); ++k_tag) {
+
+                //condition: to not check same tag against each other
+                if ((k_tag-1) != i) {
+                    if (tags[i] == tags[k_tag-1]) {
+                        found = true;
+                        break;      //break out of loop
+                    }
+                }
+            }
+
+            if (found)         //if found is true
+                Tag.push_back({tags[k_tag-1], 1, text[k_tag-1]});
+            else
+                Tag.push_back({tags[i], 1, text[i]});  
+        }
+    }
+
+    std::cout << "Testing 2" << std::endl;
     return "";
     
 }
