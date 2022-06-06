74a42ff3070683a2eaacb87e0109c6eadc9302df
Yelanika Gunasekara <yelanika.gunasekara@gmail.com>
2022-06-06 16:23:43 +0200

    Started on menu.

--- tagDriver.cpp
+++ tagDriver.cpp
@@ -25,12 +25,12 @@ int main (int argc, char* argv[]) {
         filename = argv[1];
 
     std::ifstream in(filename);
-    //std::cout << "1" << filename << std::endl;
+  
     if(!in) {
         std::cout << "Couldn't open file" << filename << std::endl;
         return 1;
     }
-    //std::cout << "2" << filename << std::endl;
+
     std::string line;
     while (std::getline(in, line)) {
         tagInfo.push_back(line);
@@ -38,15 +38,64 @@ int main (int argc, char* argv[]) {
 
     in.close();
 
-    //std::cout << "3" << filename << std::endl;
-
     if (tagInfo.size() > 0) {
-    //std::cout << "4" << filename << std::endl;
         GNSSEN002::ExtractTagsandText(tagInfo);
-        //std::cout << "5" << filename << std::endl;
     }
     else    
         std::cout << "There is no data in the file provided: " << filename << std::endl;
-    //std::cout << tagInfo.size() << std::endl;
     return 0;
+
+    for(;;) {
+
+        std::cout << "r: Read and process tag file" << std::endl;
+        std::cout << "p: Print all tags" << std::endl;
+        std::cout << "d: Write tags and data to a file called tag.txt" << std::endl;
+        std::cout << "r: Print tag for given tag" << std::endl;
+        std::cout << "q: Quit" << std::endl;
+
+        if (argc == 1) {
+            filename = "simple.txt";
+
+            std::ifstream in(filename);
+
+            if(!in) {
+                std::cout << "Couldn't open file" << filename << std::endl;
+                return 1;
+            }
+
+            std::string line;
+            while (std::getline(in, line)) {
+                tagInfo.push_back(line);
+            }
+
+            in.close();
+
+        }
+        else {
+            filename = argv[1];
+
+            std::ifstream in(filename);
+
+            switch (checkOptions) {                 //case statement holding all the different available options 
+         
+                case 'r' : {
+                    break;
+                }
+                case 'p' : {
+                    break;
+                }
+                case 'd' : {
+                    break;
+                }
+                case 'l' : {
+                    break;
+                }
+                case 'q' : {
+                    break;
+                }
+                default:
+                    break;
+            }
+        } 
+    }
 }
