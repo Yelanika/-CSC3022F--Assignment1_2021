16516569bd9fc9389196a383cb65f2188172b8c8
Yelanika Gunasekara <yelanika.gunasekara@gmail.com>
2022-06-06 18:59:00 +0200

    menu, quit option complete.

--- tagDriver.cpp
+++ tagDriver.cpp
@@ -16,86 +16,86 @@
 
 int main (int argc, char* argv[]) {
 
-    std::vector<std::string> tagInfo;
-    std::string filename = "simple.txt";
+    // std::vector<std::string> tagInfo;
+    // std::string filename = "simple.txt";
 
-    if (argc == 1)
-        filename = "simple.txt";
-    else
-        filename = argv[1];
+    // if (argc == 1)
+    //     filename = "simple.txt";
+    // else
+    //     filename = argv[1];
 
-    std::ifstream in(filename);
+    // std::ifstream in(filename);
   
-    if(!in) {
-        std::cout << "Couldn't open file" << filename << std::endl;
-        return 1;
-    }
+    // if(!in) {
+    //     std::cout << "Couldn't open file" << filename << std::endl;
+    //     return 1;
+    // }
 
-    std::string line;
-    while (std::getline(in, line)) {
-        tagInfo.push_back(line);
-    }
+    // std::string line;
+    // while (std::getline(in, line)) {
+    //     tagInfo.push_back(line);
+    // }
 
-    in.close();
+    // in.close();
 
-    if (tagInfo.size() > 0) {
-        GNSSEN002::ExtractTagsandText(tagInfo);
-    }
-    else    
-        std::cout << "There is no data in the file provided: " << filename << std::endl;
-    return 0;
+    // if (tagInfo.size() > 0) {
+    //     GNSSEN002::ExtractTagsandText(tagInfo);
+    // }
+    // else    
+    //     std::cout << "There is no data in the file provided: " << filename << std::endl;
+    // return 0;
 
-    for(;;) {
+    
+    char option = 'm';      // m - Menu
+    std::string filename = "";        
 
-        std::cout << "r: Read and process tag file" << std::endl;
-        std::cout << "p: Print all tags" << std::endl;
-        std::cout << "d: Write tags and data to a file called tag.txt" << std::endl;
-        std::cout << "r: Print tag for given tag" << std::endl;
-        std::cout << "q: Quit" << std::endl;
+    if (argc == 1) {        //No parameters should be entered
 
-        if (argc == 1) {
-            filename = "simple.txt";
+        for(;;) {         //Infinite loop
+            switch (option) {                 //case statement holding all the different available options 
+        
+                case 'm' : {        // the Menu
+                    std::cout << "r: Read and process tag file" << std::endl;
+                    std::cout << "p: Print all tags" << std::endl;
+                    std::cout << "d: Write tags and data to a file called tag.txt" << std::endl;
+                    std::cout << "r: Print tag for given tag" << std::endl;
+                    std::cout << "q: Quit" << std::endl;
 
-            std::ifstream in(filename);
+                    std::cin >> option;
 
-            if(!in) {
-                std::cout << "Couldn't open file" << filename << std::endl;
-                return 1;
-            }
-
-            std::string line;
-            while (std::getline(in, line)) {
-                tagInfo.push_back(line);
-            }
-
-            in.close();
-
-        }
-        else {
-            filename = argv[1];
-
-            std::ifstream in(filename);
-
-            switch (checkOptions) {                 //case statement holding all the different available options 
-         
-                case 'r' : {
                     break;
                 }
-                case 'p' : {
+                case 'r' : {        //User input of tag file
+
+                    std::cin >> filename;
+
+
+
+                    option = 'm';
                     break;
                 }
-                case 'd' : {
+                case 'p' : {        // just printing the tags
+
+                    option = 'm';
+                    break;
+                }
+                case 'd' : {        //writing tags and data to file(tag.txt)
+
+                    
+                    option = 'm';
                     break;
                 }
                 case 'l' : {
+
+                    option = 'm';
                     break;
                 }
-                case 'q' : {
-                    break;
+                case 'q' : {    //End the program
+                    return 1;
                 }
                 default:
                     break;
             }
-        } 
-    }
+        }
+    } 
 }
